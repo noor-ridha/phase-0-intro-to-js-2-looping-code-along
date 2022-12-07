de3ac6e57080778c6f20d965afabc76cd6212e63
noor <noorridha028@gmail.com>
2022-12-07 12:22:44 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function writeCards(arr, eventName) {
+    let cardArray = [];
+    for(let i = 0; i < arr.length; i++) {
+        cardArray.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return cardArray;
+};
+
+writeCards(["Guadalupe", "Ollie", "Aki"], 'birthday');
+
+
+
+function countDown(num) {
+
+    while( num >= 0 ) {
+
+        console.log(num);
+        num --;
+    }
+};
+
+countDown(10);
